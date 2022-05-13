2ae837875a5a01b6c58ce3bb77a35a8e3ef9390f
alphanication <usa.alphanication@gmail.com>
2022-05-13 19:26:13 +0300

    view MainScreen

--- app/src/main/java/com/example/compnote/screens/StartScreen.kt
+++ app/src/main/java/com/example/compnote/screens/StartScreen.kt
@@ -7,12 +7,9 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.example.compnote.navigation.NavRoute
-import com.example.compnote.ui.theme.CompnoteTheme
 
 @Composable
 fun StartScreen(navController: NavController) {
@@ -27,7 +24,7 @@ fun StartScreen(navController: NavController) {
             Text(text = "What will we use?")
             Button(
                 onClick = {
-                          navController.navigate(route = NavRoute.Main.route)
+                    navController.navigate(route = NavRoute.Main.route)
                 },
                 modifier = Modifier
                     .width(200.dp)
@@ -46,13 +43,5 @@ fun StartScreen(navController: NavController) {
                 Text(text = "FIREBASE DATABASE")
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun PrevStartScreen() {
-    CompnoteTheme {
-        StartScreen(navController = rememberNavController())
     }
 }
