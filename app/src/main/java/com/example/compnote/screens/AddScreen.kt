684966831ce5744c921917d4a7d177eff3bc7aca
alphanication <usa.alphanication@gmail.com>
2022-05-13 23:49:48 +0300

    view AddScreen

--- app/src/main/java/com/example/compnote/screens/AddScreen.kt
+++ app/src/main/java/com/example/compnote/screens/AddScreen.kt
@@ -1,9 +1,54 @@
 package com.example.compnote.screens
 
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.compnote.navigation.NavRoute
 
 @Composable
 fun AddScreen(navController: NavController) {
-
+    var title by remember { mutableStateOf("") }
+    var subtitle by remember { mutableStateOf("") }
+    Scaffold {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Add new note",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(8.dp)
+            )
+            OutlinedTextField(
+                value = title,
+                onValueChange = { title = it },
+                label = { Text(text = "Note title") }
+            )
+            OutlinedTextField(
+                value = subtitle,
+                onValueChange = { subtitle = it },
+                label = { Text(text = "Note Subtitle") }
+            )
+            Button(
+                modifier = Modifier.padding(top = 16.dp),
+                onClick = { navController.navigate(NavRoute.Main.route) }
+            ) {
+                Text(text = "ADD NOTE")
+            }
+        }
+    }
 }
