1333a557ce620742ac59948b89cfc59000bc321c
alphanication <usa.alphanication@gmail.com>
2022-05-13 19:26:13 +0300

    view MainScreen

--- app/src/main/java/com/example/compnote/screens/MainScreen.kt
+++ app/src/main/java/com/example/compnote/screens/MainScreen.kt
@@ -1,9 +1,69 @@
 package com.example.compnote.screens
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.compnote.navigation.NavRoute
 
 @Composable
 fun MainScreen(navController: NavController) {
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate(NavRoute.Add.route)
+                },
+                backgroundColor = Color.Blue,
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = "Add icons",
+                    tint = Color.White
+                )
+            }
+        },
+        modifier = Modifier.padding(bottom = 8.dp, end = 8.dp)
+    ) {
+        Column {
+            NoteItem(navController = navController, title = "Note 1", subtitle = "Subtitle for note 2")
+        }
 
+    }
+}
+
+@Composable
+fun NoteItem(navController: NavController, title: String, subtitle: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp, horizontal = 24.dp)
+            .clickable {
+                navController.navigate(NavRoute.Note.route)
+            },
+        elevation = 6.dp
+    ) {
+        Column(
+            modifier = Modifier.padding(vertical = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = title,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+            Text(text = subtitle)
+        }
+    }
 }
