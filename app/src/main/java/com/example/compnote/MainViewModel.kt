37b39c9691d38f62ff07385351e9c75e0ffda722
alphanication <usa.alphanication@gmail.com>
2022-05-14 20:10:30 +0300

    correct navigation, click database

--- app/src/main/java/com/example/compnote/MainViewModel.kt
+++ app/src/main/java/com/example/compnote/MainViewModel.kt
@@ -5,30 +5,25 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.compnote.database.room.AppRoomDatabase
+import com.example.compnote.database.room.repository.RoomRepository
 import com.example.compnote.models.Note
+import com.example.compnote.util.REPOSITORY
 import com.example.compnote.util.TYPE_FIREBASE
 import com.example.compnote.util.TYPE_ROOM
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
 
-    val readTest = MutableLiveData<List<Note>>()
+    val context = application
 
-    private val dbType = MutableLiveData(TYPE_ROOM)
-
-    init {
-        readTest.value = when (dbType.value) {
-            TYPE_ROOM -> listOf(
-                Note(title = "Note 1", subtitle = "Subtitle for note 1"),
-                Note(title = "Note 2", subtitle = "Subtitle for note 2"),
-                Note(title = "Note 3", subtitle = "Subtitle for note 3")
-            )
-            TYPE_FIREBASE -> listOf()
-            else -> listOf()
+    fun initDatabase(type: String, onSuccess: () -> Unit) {
+        when(type) {
+            TYPE_ROOM -> {
+                val dao = AppRoomDatabase.getInstance(context = context).getRoomDao()
+                REPOSITORY = RoomRepository(dao)
+                onSuccess()
+            }
         }
-    }
-
-    fun initDatabase(type: String) {
-        dbType.value = type
     }
 }
 
--- app/src/main/java/com/example/compnote/screens/StartScreen.kt
+++ app/src/main/java/com/example/compnote/screens/StartScreen.kt
@@ -35,8 +35,9 @@ fun StartScreen(navController: NavController) {
             Text(text = "What will we use?")
             Button(
                 onClick = {
-                    mViewModel.initDatabase(TYPE_ROOM)
-                    navController.navigate(route = NavRoute.Main.route)
+                    mViewModel.initDatabase(TYPE_ROOM) {
+                        navController.navigate(route = NavRoute.Main.route)
+                    }
                 },
                 modifier = Modifier
                     .width(200.dp)
@@ -46,8 +47,9 @@ fun StartScreen(navController: NavController) {
             }
             Button(
                 onClick = {
-                    mViewModel.initDatabase(TYPE_FIREBASE)
-                    navController.navigate(route = NavRoute.Main.route)
+                    mViewModel.initDatabase(TYPE_FIREBASE) {
+                        navController.navigate(route = NavRoute.Main.route)
+                    }
                 },
                 modifier = Modifier
                     .width(200.dp)
