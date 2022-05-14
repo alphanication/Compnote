d79b42f62524c7bd01e55bbe6d7274541c06633c
alphanication <usa.alphanication@gmail.com>
2022-05-14 19:21:07 +0300

    MainScreen, default ROOM storage

--- app/src/main/java/com/example/compnote/MainViewModel.kt
+++ app/src/main/java/com/example/compnote/MainViewModel.kt
@@ -1,14 +1,34 @@
 package com.example.compnote
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.compnote.models.Note
+import com.example.compnote.util.TYPE_FIREBASE
+import com.example.compnote.util.TYPE_ROOM
 
 class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+    val readTest = MutableLiveData<List<Note>>()
+
+    private val dbType = MutableLiveData(TYPE_ROOM)
+
+    init {
+        readTest.value = when (dbType.value) {
+            TYPE_ROOM -> listOf(
+                Note(title = "Note 1", subtitle = "Subtitle for note 1"),
+                Note(title = "Note 2", subtitle = "Subtitle for note 2"),
+                Note(title = "Note 3", subtitle = "Subtitle for note 3")
+            )
+            TYPE_FIREBASE -> listOf()
+            else -> listOf()
+        }
+    }
+
     fun initDatabase(type: String) {
-        Log.d("alpha33", "vm main init with type: $type")
+        dbType.value = type
     }
 }
 
--- app/src/main/java/com/example/compnote/screens/MainScreen.kt
+++ app/src/main/java/com/example/compnote/screens/MainScreen.kt
@@ -1,24 +1,40 @@
 package com.example.compnote.screens
 
+import android.app.Application
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.compnote.MainViewModel
+import com.example.compnote.MainViewModelFactory
+import com.example.compnote.models.Note
 import com.example.compnote.navigation.NavRoute
 
 @Composable
 fun MainScreen(navController: NavController) {
+
+    val context = LocalContext.current
+    val mViewModel: MainViewModel =
+        viewModel(factory = MainViewModelFactory(context.applicationContext as Application))
+
+    val notes = mViewModel.readTest.observeAsState(listOf()).value
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -36,15 +52,16 @@ fun MainScreen(navController: NavController) {
         },
         modifier = Modifier.padding(bottom = 8.dp, end = 8.dp)
     ) {
-        Column {
-            NoteItem(navController = navController, title = "Note 1", subtitle = "Subtitle for note 2")
+        LazyColumn {
+            items(notes) { note ->
+                NoteItem(navController = navController, note = note)
+            }
         }
-
     }
 }
 
 @Composable
-fun NoteItem(navController: NavController, title: String, subtitle: String) {
+fun NoteItem(navController: NavController, note: Note) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -59,11 +76,11 @@ fun NoteItem(navController: NavController, title: String, subtitle: String) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = title,
+                text = note.title,
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold
             )
-            Text(text = subtitle)
+            Text(text = note.subtitle)
         }
     }
 }
