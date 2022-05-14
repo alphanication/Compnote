93c4766dc72417ca66179c53c0ef6bd5140e68f1
alphanication <usa.alphanication@gmail.com>
2022-05-14 17:38:56 +0300

    Entity Note-model

--- app/src/main/java/com/example/compnote/models/Note.kt
+++ app/src/main/java/com/example/compnote/models/Note.kt
@@ -1,7 +1,15 @@
 package com.example.compnote.models
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "notes_table")
 data class Note(
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
+    @ColumnInfo
     val title: String,
+    @ColumnInfo
     val subtitle: String
 )
