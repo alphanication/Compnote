47248e0df91e4344b95b44e508b86119d5733433
alphanication <usa.alphanication@gmail.com>
2022-05-15 12:52:20 +0300

    shows all notes MainScreen

--- app/src/main/java/com/example/compnote/screens/MainScreen.kt
+++ app/src/main/java/com/example/compnote/screens/MainScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,6 +24,8 @@ import com.example.compnote.navigation.NavRoute
 
 @Composable
 fun MainScreen(navController: NavController, viewModel: MainViewModel) {
+    val notes = viewModel.readAllNotes().observeAsState(listOf()).value
+
     Scaffold(
         floatingActionButton = {
             FloatingActionButton(
@@ -38,7 +43,11 @@ fun MainScreen(navController: NavController, viewModel: MainViewModel) {
         },
         modifier = Modifier.padding(bottom = 8.dp, end = 8.dp)
     ) {
-
+        LazyColumn {
+            items(notes) { note ->
+                NoteItem(navController = navController, note = note)
+            }
+        }
     }
 }
 
