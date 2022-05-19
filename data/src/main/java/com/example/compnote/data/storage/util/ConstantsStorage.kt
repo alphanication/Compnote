e5159dc70ee7f9aa44f777b0958af6a21a64dd92
alphanication <usa.alphanication@gmail.com>
2022-05-19 18:54:41 +0300

    rename data Constants to ConstantsStorage

--- data/src/main/java/com/example/compnote/data/storage/util/ConstantsStorage.kt
+++ data/src/main/java/com/example/compnote/data/storage/util/ConstantsStorage.kt
@@ -1,6 +1,6 @@
 package com.example.compnote.data.storage.util
 
-object Constants {
+object ConstantsStorage {
     object Keys {
         const val NOTES_DATABASE = "notes_database"
         const val NOTES_TABLE = "notes_table"
