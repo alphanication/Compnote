20b867af570ff0ecb462bc9faa53d51298e4468e
alphanication <usa.alphanication@gmail.com>
2022-05-21 21:21:48 +0300

    noteitem border as primary color, cutcorner

--- app/src/main/java/com/example/compnote/presentation/screens/main/MainScreen.kt
+++ app/src/main/java/com/example/compnote/presentation/screens/main/MainScreen.kt
@@ -1,16 +1,19 @@
 package com.example.compnote.presentation.screens.main
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,15 +26,18 @@ import androidx.navigation.NavController
 import com.example.compnote.domain.models.Note
 import com.example.compnote.presentation.navigation.NavRoute
 
+
 @Composable
 fun MainScreen(navController: NavController) {
     val mViewModel = hiltViewModel<MainViewModel>()
 
     val notes = mViewModel.allListNote.observeAsState(listOf()).value
 
-    LaunchedEffect(key1 = Unit, block = {
+    DisposableEffect(Unit) {
         mViewModel.getAllNotes()
-    })
+
+        onDispose { mViewModel.clearAllListNoteLiveData() }
+    }
 
     Scaffold(
         floatingActionButton = {
@@ -66,18 +72,24 @@ fun NoteItem(navController: NavController, note: Note) {
             .padding(vertical = 8.dp, horizontal = 24.dp)
             .clickable {
                 navController.navigate(NavRoute.NoteScreen.route + "/${note.id}")
-            }
+            },
+        shape = CutCornerShape(topEnd = 20.dp, bottomStart = 20.dp),
+        border = BorderStroke(2.dp, MaterialTheme.colors.primary)
     ) {
         Column(
             modifier = Modifier.padding(vertical = 8.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
+                modifier = Modifier.padding(horizontal = 10.dp),
                 text = note.title,
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
             )
-            Text(text = note.subtitle)
+
+            Text(text = note.subtitle, modifier = Modifier.padding(horizontal = 10.dp))
+
+            Spacer(modifier = Modifier.padding(bottom = 10.dp))
         }
     }
 }
