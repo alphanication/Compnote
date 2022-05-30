2b91283d95c145a70b59a0f8639c2511c7d369f3
alphanication <usa.alphanication@gmail.com>
2022-05-30 16:46:38 +0300

    view-viewmodel correction

--- app/src/main/java/com/example/compnote/presentation/screens/add/AddViewModel.kt
+++ app/src/main/java/com/example/compnote/presentation/screens/add/AddViewModel.kt
@@ -1,6 +1,9 @@
 package com.example.compnote.presentation.screens.add
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.compnote.domain.models.Note
 import com.example.compnote.domain.models.Response
 import com.example.compnote.domain.usecase.NoteAddUseCase
@@ -9,6 +12,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -16,13 +20,18 @@ class AddViewModel @Inject constructor(
     private val noteAddUseCase: NoteAddUseCase,
 ) : ViewModel() {
 
-    fun addNote(note: Note): Flow<Boolean> = flow {
-        noteAddUseCase.execute(note = note).collect { response ->
-            when (response) {
-                is Response.Loading -> {}
-                is Response.Fail -> emit(false)
-                is Response.Success -> emit(response.data)
+    private val _addNoteResult = MutableLiveData<Boolean>()
+    val addNoteResult: LiveData<Boolean> = _addNoteResult
+
+    fun addNote(note: Note) {
+        viewModelScope.launch(Dispatchers.IO) {
+            noteAddUseCase.execute(note = note).collect { response ->
+                when (response) {
+                    is Response.Loading -> {}
+                    is Response.Fail -> _addNoteResult.postValue(false)
+                    is Response.Success -> _addNoteResult.postValue(true)
+                }
             }
         }
-    }.flowOn(Dispatchers.IO)
+    }
 }
--- app/src/main/java/com/example/compnote/presentation/screens/note/NoteScreen.kt
+++ app/src/main/java/com/example/compnote/presentation/screens/note/NoteScreen.kt
@@ -24,21 +24,32 @@ import com.example.compnote.presentation.ui.theme.backgroundContentButton
 import com.example.compnote.presentation.util.Constants
 import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalMaterialApi::class)
 @Composable
+@OptIn(ExperimentalMaterialApi::class)
 fun NoteScreen(navController: NavController, noteId: String?) {
 
     val mViewModel = hiltViewModel<NoteViewModel>()
 
     val note = mViewModel.note.observeAsState(
-        Note(
-            title = Constants.Keys.NONE,
-            subtitle = Constants.Keys.NONE
-        )
+            Note(
+                    title = Constants.Keys.NONE,
+                    subtitle = Constants.Keys.NONE
+            )
     ).value
+
+    val resultUpdateNote = mViewModel.resultUpdateNote.observeAsState(false).value
+    val resultDeleteNote = mViewModel.resultDeleteNote.observeAsState(false).value
 
     LaunchedEffect(key1 = Unit, block = {
         if (noteId?.isNotEmpty() == true) mViewModel.getNoteById(id = noteId.toInt())
+    })
+
+    LaunchedEffect(key1 = resultUpdateNote, block = {
+        if (resultUpdateNote) navController.navigate(NavRoute.MainScreen.route)
+    })
+
+    LaunchedEffect(key1 = resultDeleteNote, block = {
+        if (resultDeleteNote) navController.navigate(NavRoute.MainScreen.route)
     })
 
     val coroutineScope = rememberCoroutineScope()
@@ -50,132 +61,128 @@ fun NoteScreen(navController: NavController, noteId: String?) {
     var isButtonEnabled by remember { mutableStateOf(false) }
 
     ModalBottomSheetLayout(
-        sheetState = bottomSheetState,
-        sheetBackgroundColor = MaterialTheme.colors.background,
-        sheetElevation = 5.dp,
-        sheetShape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp),
-        sheetContent = {
-            Surface {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(MaterialTheme.colors.background)
-                        .padding(25.dp)
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .heightIn(0.dp, 100.dp)
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween,
-                        verticalAlignment = Alignment.CenterVertically
+            sheetState = bottomSheetState,
+            sheetBackgroundColor = MaterialTheme.colors.background,
+            sheetElevation = 5.dp,
+            sheetShape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp),
+            sheetContent = {
+                Surface {
+                    Column(
+                            modifier = Modifier
+                                    .fillMaxSize()
+                                    .background(MaterialTheme.colors.background)
+                                    .padding(25.dp)
                     ) {
-                        Text(
-                            text = Constants.Keys.EDIT_NOTE,
-                            fontSize = 18.sp,
-                            fontWeight = FontWeight.Bold,
+                        Row(
+                                modifier = Modifier
+                                        .heightIn(0.dp, 100.dp)
+                                        .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.SpaceBetween,
+                                verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                    text = Constants.Keys.EDIT_NOTE,
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Bold,
+                            )
+
+                            Button(
+                                    enabled = isButtonEnabled,
+                                    onClick = {
+                                        if (title.isNotEmpty() and subtitle.isNotEmpty()) {
+                                            mViewModel.updateNote(
+                                                    note = Note(
+                                                            id = note.id,
+                                                            title = title,
+                                                            subtitle = subtitle
+                                                    )
+                                            )
+                                        }
+                                    },
+                                    shape = RoundedCornerShape(15.dp)
+                            ) {
+                                Text(text = Constants.Keys.UPDATE_NOTE)
+                            }
+                        }
+
+                        OutlinedTextField(
+                                modifier = Modifier.fillMaxWidth(),
+                                value = title,
+                                onValueChange = {
+                                    title = it
+                                    isButtonEnabled = title.isNotEmpty() && subtitle.isNotEmpty()
+                                },
+                                label = { Text(text = Constants.Keys.TITLE) },
+                                trailingIcon = {
+                                    if (title.isEmpty()) Icon(
+                                            Icons.Filled.Info,
+                                            contentDescription = "error",
+                                            tint = Color.Red
+                                    )
+                                },
+                                isError = title.isEmpty(),
+                                colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = MaterialTheme.colors.surface),
+                                shape = RoundedCornerShape(25.dp)
                         )
 
-                        Button(
-                            enabled = isButtonEnabled,
-                            onClick = {
-                                if (title.isNotEmpty() and subtitle.isNotEmpty()) {
-                                    coroutineScope.launch {
-                                        mViewModel.updateNote(
-                                            note = Note(
-                                                id = note.id,
-                                                title = title,
-                                                subtitle = subtitle
-                                            )
-                                        ).collect {
-                                            if (it) navController.navigate(NavRoute.MainScreen.route)
-                                        }
-                                    }
-                                }
-                            },
-                            shape = RoundedCornerShape(15.dp)
-                        ) {
-                            Text(text = Constants.Keys.UPDATE_NOTE)
-                        }
+                        Spacer(modifier = Modifier.padding(top = 5.dp))
+
+                        OutlinedTextField(
+                                modifier = Modifier.fillMaxWidth(),
+                                value = subtitle,
+                                onValueChange = {
+                                    subtitle = it
+                                    isButtonEnabled = title.isNotEmpty() && subtitle.isNotEmpty()
+                                },
+                                label = { Text(text = Constants.Keys.SUBTITLE) },
+                                isError = subtitle.isEmpty(),
+                                colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = MaterialTheme.colors.surface),
+                                shape = RoundedCornerShape(25.dp)
+                        )
+
+                        Spacer(modifier = Modifier.padding(bottom = 15.dp))
                     }
-
-                    OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = title,
-                        onValueChange = {
-                            title = it
-                            isButtonEnabled = title.isNotEmpty() && subtitle.isNotEmpty()
-                        },
-                        label = { Text(text = Constants.Keys.TITLE) },
-                        trailingIcon = {
-                            if (title.isEmpty()) Icon(
-                                Icons.Filled.Info,
-                                contentDescription = "error",
-                                tint = Color.Red
-                            )
-                        },
-                        isError = title.isEmpty(),
-                        colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = MaterialTheme.colors.surface),
-                        shape = RoundedCornerShape(25.dp)
-                    )
-
-                    Spacer(modifier = Modifier.padding(top = 5.dp))
-
-                    OutlinedTextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = subtitle,
-                        onValueChange = {
-                            subtitle = it
-                            isButtonEnabled = title.isNotEmpty() && subtitle.isNotEmpty()
-                        },
-                        label = { Text(text = Constants.Keys.SUBTITLE) },
-                        isError = subtitle.isEmpty(),
-                        colors = TextFieldDefaults.outlinedTextFieldColors(backgroundColor = MaterialTheme.colors.surface),
-                        shape = RoundedCornerShape(25.dp)
-                    )
-
-                    Spacer(modifier = Modifier.padding(bottom = 15.dp))
                 }
             }
-        }
     ) {
         Column(
-            modifier = Modifier.fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
         ) {
             Spacer(modifier = Modifier.padding(top = 20.dp))
 
             Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .heightIn(100.dp, 500.dp)
-                    .padding(horizontal = 15.dp),
-                backgroundColor = MaterialTheme.colors.surface,
-                shape = RoundedCornerShape(25.dp),
-                border = BorderStroke(2.dp, MaterialTheme.colors.primary)
+                    modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(100.dp, 500.dp)
+                            .padding(horizontal = 15.dp),
+                    backgroundColor = MaterialTheme.colors.surface,
+                    shape = RoundedCornerShape(25.dp),
+                    border = BorderStroke(2.dp, MaterialTheme.colors.primary)
             ) {
                 LazyColumn(
-                    modifier = Modifier.padding(horizontal = 10.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
+                        modifier = Modifier.padding(horizontal = 10.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
                 ) {
                     item {
                         Spacer(modifier = Modifier.padding(top = 10.dp))
 
                         Text(
-                            modifier = Modifier.padding(horizontal = 5.dp),
-                            text = note.title,
-                            fontSize = 32.sp,
-                            fontWeight = FontWeight.Bold,
-                            maxLines = 1
+                                modifier = Modifier.padding(horizontal = 5.dp),
+                                text = note.title,
+                                fontSize = 32.sp,
+                                fontWeight = FontWeight.Bold,
+                                maxLines = 1
                         )
 
                         Spacer(modifier = Modifier.padding(top = 10.dp))
 
                         Text(
-                            text = note.subtitle,
-                            fontSize = 18.sp,
-                            fontWeight = FontWeight.Light,
-                            modifier = Modifier.padding(horizontal = 5.dp)
+                                text = note.subtitle,
+                                fontSize = 18.sp,
+                                fontWeight = FontWeight.Light,
+                                modifier = Modifier.padding(horizontal = 5.dp)
                         )
 
                         Spacer(modifier = Modifier.padding(top = 10.dp))
@@ -183,38 +190,32 @@ fun NoteScreen(navController: NavController, noteId: String?) {
                 }
             }
             Row(
-                modifier = Modifier
-                    .padding(25.dp)
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround
+                    modifier = Modifier
+                            .padding(25.dp)
+                            .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceAround
             ) {
                 Button(
-                    onClick = {
-                        coroutineScope.launch {
-                            title = note.title
-                            subtitle = note.subtitle
-                            bottomSheetState.animateTo(ModalBottomSheetValue.Expanded)
-                        }
-                    },
-                    shape = RoundedCornerShape(15.dp),
-                    colors = ButtonDefaults.buttonColors(contentColor = backgroundContentButton)
+                        onClick = {
+                            coroutineScope.launch {
+                                title = note.title
+                                subtitle = note.subtitle
+                                bottomSheetState.animateTo(ModalBottomSheetValue.Expanded)
+                            }
+                        },
+                        shape = RoundedCornerShape(15.dp),
+                        colors = ButtonDefaults.buttonColors(contentColor = backgroundContentButton)
                 ) {
                     Text(text = Constants.Keys.UPDATE)
                 }
 
                 Button(
-                    onClick = {
-                        coroutineScope.launch {
+                        onClick = {
                             mViewModel.deleteNoteById(id = note.id)
-                                .collect {
-                                    if (it) navController.navigate(NavRoute.MainScreen.route)
-                                }
-
-                        }
-                    },
-                    shape = RoundedCornerShape(15.dp),
-                    colors = ButtonDefaults.buttonColors(contentColor = backgroundContentButton)
+                        },
+                        shape = RoundedCornerShape(15.dp),
+                        colors = ButtonDefaults.buttonColors(contentColor = backgroundContentButton)
                 ) {
                     Text(text = Constants.Keys.DELETE)
                 }
